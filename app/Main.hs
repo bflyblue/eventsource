2aba2d0db6f1fcd2902508b59ca73fe99a27a6b5
Shaun Sharples <shaun.sharples@gmail.com>
2016-02-10 14:32:26 +0200

    playing with opaleye

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE Arrows            #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Control.Arrow
+import           Data.Text
+import           Database.PostgreSQL.Simple (connectPostgreSQL)
+import           Opaleye
+import           People.Person
+
+personByName :: Text -> Query PersonColumn
+personByName name = proc () -> do
+    p <- personQuery -< ()
+    restrict -< (personName p .== pgStrictText name)
+    returnA -< p
 
 main :: IO ()
-main = startApp
+main = do
+    conn <- connectPostgreSQL "dbname='people'"
+    ps <- runQuery conn (personByName "Joe Soap")
+    print (ps :: [Person])
+    joeid <- runInsert conn peopleTable Person { personId = Nothing, personName = pgStrictText "Joe Soap" }
+    print joeid
