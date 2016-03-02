a5085202a6fa4b82fe28f365250ee834b44ada38
Shaun Sharples <shaun.sharples@gmail.com>
2016-03-02 17:51:25 +0200

    testing

--- src/Datastore/Command.hs
+++ src/Datastore/Command.hs
@@ -1,8 +1,24 @@
 module Datastore.Command where
 
+import           Control.Monad.Trans.Either
+import           Control.Monad.Trans.Reader
 import           Data.Text                  (Text)
+import           Database.PostgreSQL.Simple
 
-type Command = IO
+data CmdErr = CmdErr
+
+data CmdEnv = CmdEnv { connection :: Connection }
+
+type Command = ReaderT CmdEnv (EitherT CmdErr IO)
+
+runCommand :: CmdEnv -> Command a -> IO (Either CmdErr a)
+runCommand env cmd = runEitherT $ runReaderT cmd env
+
+getConnection :: Command Connection
+getConnection = asks connection
 
 createPerson :: Text -> Command Int
-createPerson = undefined
+createPerson _name = do
+    _conn <- getConnection
+    -- liftIO $ runInsert conn
+    return 1
