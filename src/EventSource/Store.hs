30b75220242d02a931f43c2754b1df4acfd72dec
Shaun Sharples <shaun.sharples@gmail.com>
2016-03-09 16:13:53 +0200

    deleted unused files

--- src/EventSource/Store.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE TypeFamilies          #-}
-
-module EventSource.Store where
-
-import Control.Monad.IO.Class
-
-class Store s where
-    transaction :: MonadIO m => s a -> m (Either String a)
-    rollback :: String -> s a
